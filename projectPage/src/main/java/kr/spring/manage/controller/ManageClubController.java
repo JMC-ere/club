6f335aa986ea21d391a52cf63cde63c22d30789d
user <user@Si7C-07>
2020-08-04 17:26:34 +0900

    4 up

--- projectPage/src/main/java/kr/spring/manage/controller/ManageClubController.java
+++ projectPage/src/main/java/kr/spring/manage/controller/ManageClubController.java
@@ -19,6 +19,7 @@ import kr.spring.manage.domain.ManageClubVO;
 import kr.spring.manage.service.ManageClubService;
 import kr.spring.util.PagingUtil;
 
+//아무거나
 @Controller
 public class ManageClubController {
 	private Logger log = Logger.getLogger(this.getClass());
