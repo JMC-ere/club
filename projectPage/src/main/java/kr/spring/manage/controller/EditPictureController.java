f574c0d0b0c89377557cdcf39bb147529071d7d2
h92218 <sh717510@gmail.com>
2020-08-10 16:10:41 +0900

    123

--- projectPage/src/main/java/kr/spring/manage/controller/EditPictureController.java
+++ projectPage/src/main/java/kr/spring/manage/controller/EditPictureController.java
@@ -1,8 +1,13 @@
 package kr.spring.manage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class EditPictureController {
 
+	@RequestMapping("EditPicture.do")
+	public String editpicture() {
+		return "EditPicture";
+	}
 }
